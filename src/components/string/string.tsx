cf8a456071cd73ee69522d2770fe19d3297c9477
pomazkovanton <vitrumbeta@gmail.com>
2023-03-14 11:44:31 +0800

    feat(string): добавить отрисовку строки на страницу

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,27 +1,39 @@
 import React, { useState } from "react";
-import clc from "./string.module.css";
+import cls from "./string.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const StringComponent: React.FC = () => {
-  const [valueInput, setValueInput] = useState("");
+  const [valueInput, setValueInput] = useState<null | string>(null);
+  const [arrChars, setArrChars] = useState<null | string[]>(null);
 
   const reverseString = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(valueInput);
+    valueInput && setArrChars(valueInput?.split(""));
   };
 
   return (
     <SolutionLayout title="Строка">
-      <form className={clc.form} onSubmit={(e) => reverseString(e)}>
+      <form className={cls.form} onSubmit={(e) => reverseString(e)}>
         <Input
           maxLength={11}
           isLimitText={true}
           onChange={(e) => setValueInput(e.currentTarget.value)}
         />
-        <Button text="Развернуть" type="submit" disabled={valueInput === ""} />
+        <Button text="Развернуть" type="submit" disabled={!valueInput} />
       </form>
+      <ul className={cls.list}>
+        {arrChars &&
+          arrChars.map((char, index) => {
+            return (
+              <li key={index}>
+                <Circle letter={char} />
+              </li>
+            );
+          })}
+      </ul>
     </SolutionLayout>
   );
 };
