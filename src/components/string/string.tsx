3b25c98574f4bb7ad2e56ec95e4c05e6da41246f
pomazkovanton <vitrumbeta@gmail.com>
2023-03-15 13:04:19 +0800

    feat(string): добавить функцию реверса строки

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -4,14 +4,34 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
+import { delay } from "../../utils/utils";
 
 export const StringComponent: React.FC = () => {
-  const [valueInput, setValueInput] = useState<null | string>(null);
-  const [arrChars, setArrChars] = useState<null | string[]>(null);
+  const [valueInput, setValueInput] = useState<string>("");
+  const [arrChars, setArrChars] = useState<string[]>([]);
+  const [isReverseString, setIsReverseString] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const reverseString = (e: React.FormEvent<HTMLFormElement>) => {
+  const reverseString = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    valueInput && setArrChars(valueInput?.split(""));
+    setIsReverseString(false);
+    setIsLoading(true);
+    setArrChars(valueInput.split(""));
+
+    const tempArray = valueInput.split("");
+    const lengthArray = tempArray.length;
+
+    for (let i = 0; i < lengthArray / 2; i++) {
+      const tmp = tempArray[i];
+      tempArray[i] = tempArray[lengthArray - i - 1];
+      tempArray[lengthArray - i - 1] = tmp;
+    }
+
+    setArrChars(tempArray);
+    setValueInput(tempArray.join(""));
+    setIsReverseString(true);
+    setIsLoading(false);
   };
 
   return (
@@ -22,10 +42,16 @@ export const StringComponent: React.FC = () => {
           isLimitText={true}
           onChange={(e) => setValueInput(e.currentTarget.value)}
         />
-        <Button text="Развернуть" type="submit" disabled={!valueInput} />
+        <Button
+          text="Развернуть"
+          type="submit"
+          disabled={!valueInput}
+          isLoader={isLoading}
+        />
       </form>
       <ul className={cls.list}>
-        {arrChars &&
+        {isReverseString &&
+          arrChars &&
           arrChars.map((char, index) => {
             return (
               <li key={index}>
