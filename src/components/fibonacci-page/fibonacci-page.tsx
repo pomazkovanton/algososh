863f4bda686838c4e666aad37e84d6c1cc52c24d
pomazkovanton <vitrumbeta@gmail.com>
2023-03-16 11:00:07 +0800

    feat(fibonacci): добавить генератор чисел фибоначи

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -6,17 +6,38 @@ import { Button } from "../ui/button/button";
 import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Circle } from "../ui/circle/circle";
+import { delay } from "../../utils/utils";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 
 export const FibonacciPage: React.FC = () => {
   const [valueInput, setValueInput] = useState<string>("");
   const [isLoading, setIsLoading] = useState(false);
-  const [arrayFibonacci, setArrayFibonacci] = useState([
-    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597,
-    2584, 4181,
-  ]);
+  const [arrayFibonacci, setArrayFibonacci] = useState<Number[]>([]);
+
+  const generateFibonacci = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+    await delay(SHORT_DELAY_IN_MS);
+    setArrayFibonacci([0]);
+    await delay(SHORT_DELAY_IN_MS);
+    setArrayFibonacci((arrayFibonacci) => [...arrayFibonacci, 1]);
+
+    const rowLength = Number(valueInput);
+    const tempArrayFib = [0, 1];
+
+    for (let i = 2; i < rowLength + 1; i++) {
+      await delay(SHORT_DELAY_IN_MS);
+      let nextNumberFib = tempArrayFib[i - 2] + tempArrayFib[i - 1];
+      tempArrayFib.push(nextNumberFib);
+      setArrayFibonacci((arrayFibonacci) => [...arrayFibonacci, nextNumberFib]);
+    }
+    setIsLoading(false);
+  };
+
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-      <form className={cls.form} onSubmit={(e) => console.log(e)}>
+      <form className={cls.form} onSubmit={(e) => generateFibonacci(e)}>
         <Input
           min={1}
           max={19}
