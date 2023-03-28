548b8d470451ae79f7e13a21db041da7c979e1fe
pomazkovanton <vitrumbeta@gmail.com>
2023-03-28 11:09:18 +0800

    feat(list): добавить отрисовку начального списка

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,78 @@
-import React from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import cls from "./list-page.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { LinkedList } from "../../utils/linked-list";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+import { ArrowIcon } from "../ui/icons/arrow-icon";
+
+const initValueList = ["0", "34", "8", "1"];
 
 export const ListPage: React.FC = () => {
+  const [valueInput, setValueInput] = useState<string>("");
+  const [indexInput, setIndexInput] = useState<string>("");
+  const [linkedList, setLinkedList] = useState<string[]>([]);
+  const list = useRef() as MutableRefObject<LinkedList<string>>;
+
+  useEffect(() => {
+    list.current = new LinkedList<string>();
+    list.current.fromArray(initValueList);
+
+    const listNodes = list.current.toArray();
+    const arrayListValues: string[] = [];
+    listNodes.forEach((el) => arrayListValues.push(el.data));
+
+    setLinkedList(arrayListValues);
+  }, []);
+
   return (
     <SolutionLayout title="Связный список">
-
+      <div className={cls.header}>
+        <Input
+          maxLength={4}
+          isLimitText
+          placeholder={"Введите значение"}
+          style={{ minWidth: "204px" }}
+          value={valueInput}
+          onChange={(e) => setValueInput(e.currentTarget.value)}
+        />
+        <Button text="Добавить в head" style={{ minWidth: "175px" }} />
+        <Button text="Добавить в tail" style={{ minWidth: "175px" }} />
+        <Button text="Удалить из head" style={{ minWidth: "175px" }} />
+        <Button text="Удалить из tail" style={{ minWidth: "175px" }} />
+        <Input
+          min={0}
+          max={10}
+          maxLength={2}
+          type="number"
+          value={indexInput}
+          style={{ minWidth: "204px" }}
+          placeholder={"Введите индекс"}
+          onChange={(e) => setIndexInput(e.currentTarget.value)}
+        />
+        <Button text="Добавить по индексу" style={{ minWidth: "362px" }} />
+        <Button text="Удалить по индексу" style={{ minWidth: "362px" }} />
+      </div>
+      <ul className={cls.list}>
+        {linkedList.map((el, index) => {
+          return (
+            <div className={cls.item}>
+              <Circle
+                letter={el}
+                index={index}
+                state={ElementStates.Default}
+                tail={index === linkedList.length - 1 ? "tail" : ""}
+                head={index === 0 ? "head" : ""}
+              />
+              {linkedList.length > 1 && index !== linkedList.length - 1 && (
+                <ArrowIcon />
+              )}
+            </div>
+          );
+        })}
+      </ul>
     </SolutionLayout>
   );
 };
---
+++ src/utils/linked-list.ts
@@ -0,0 +1,66 @@
+export class Node<T> {
+  public data: T;
+  public next: Node<T> | null = null;
+  constructor(data: T, next: Node<T> | null = null) {
+    this.data = data;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+type ILinkedList<T> = {
+  append(data: T): void;
+  prepend(data: T): void;
+  fromArray(array: Array<T>): void;
+  toArray(): Node<T>[];
+};
+
+export class LinkedList<T> implements ILinkedList<T> {
+  private head: Node<T> | null;
+  private tail: Node<T> | null;
+  private size: number;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(value: T) {
+    const newNode = new Node(value);
+
+    if (!this.head || !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.size++;
+      return this;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.size++;
+    return this;
+  }
+
+  prepend(value: T) {
+    const newNode = new Node(value, this.head);
+    this.head = newNode;
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+    this.size++;
+    return this;
+  }
+
+  fromArray(values: Array<T>) {
+    values.forEach((value) => this.append(value));
+    return this;
+  }
+
+  toArray() {
+    const nodes = [];
+    let currentNode = this.head;
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+    return nodes;
+  }
+}
