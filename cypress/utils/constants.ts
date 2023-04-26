37e5958a32ca3ae086030ff6662b3a0f81204af0
pomazkovanton <vitrumbeta@gmail.com>
2023-04-26 20:37:55 +0800

    test(e2e): написать тест работоспособности списка

--- cypress/utils/constants.ts
+++ cypress/utils/constants.ts
@@ -11,11 +11,19 @@ export const STRING_TEST_MIDLE = "52341";
 export const STRING_TEST_END = "54321";
 
 export const INPUT_VALUE_SELECTOR = '[data-cy="input-value"]';
+export const INPUT_INDEX_SELECTOR = '[data-cy="input-index"]';
 export const BUTTON_SELECTOR = '[data-cy="button"]';
 export const BUTTON_ADD_SELECTOR = '[data-cy="button-add"]';
+export const BUTTON_ADD_IN_HEAD_SELECTOR = '[data-cy="button-add-in-head"]';
+export const BUTTON_ADD_IN_TAIL_SELECTOR = '[data-cy="button-add-in-tail"]';
+export const BUTTON_ADD_BY_INDEX_SELECTOR = '[data-cy="button-add-by-index"]';
 export const BUTTON_DELETE_SELECTOR = '[data-cy="button-del"]';
+export const BUTTON_DELETE_FROM_HEAD_SELECTOR = '[data-cy="button-del-from-head"]';
+export const BUTTON_DELETE_FROM_TAIL_SELECTOR = '[data-cy="button-del-from-tail"]';
+export const BUTTON_DELETE_BY_INDEX_SELECTOR = '[data-cy="button-del-by-index"]';
 export const BUTTON_CLEAR_SELECTOR = '[data-cy="button-clear"]';
 export const CIRCLE_SELECTOR = '[data-cy="circle-main"]';
 
 export const FIBONACCI_SERIES = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181];
-export const TEST_NUMBERS_ARRAY = [0, 1, 3, 4, 5];
+export const TEST_NUMBERS_ARRAY = [0, 1, 2, 3, 4];
+export const LIST_NUMBERS_ARRAY = [0, 34, 8, 1];
